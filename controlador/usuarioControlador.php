6103406db980d7f44272957f86cc576962ffcb62
dianaMiranda14 <dvmiranda0@misena.edu.co>
2018-08-25 21:04:43 -0500

    21:04

--- controlador/usuarioControlador.php
+++ controlador/usuarioControlador.php
@@ -1,1 +1,13 @@
-usuarioControlador.php
+<?php
+	include_once("../modelo/conexion.php");
+	include_once("../modelo/usuario.php");
+
+	class usuarioControlador{
+		private $objUsuario;
+		private $objConexion;
+
+
+		
+	}
+
+?>
