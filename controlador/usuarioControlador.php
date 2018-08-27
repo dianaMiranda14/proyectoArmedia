211c818231246a8af99cb1d86ba558be1114d1e6
dianaMiranda14 <dvmiranda0@misena.edu.co>
2018-08-27 09:37:47 -0500

    9:37

--- controlador/usuarioControlador.php
+++ controlador/usuarioControlador.php
@@ -1,13 +1,108 @@
 <?php
-	include_once("../modelo/conexion.php");
 	include_once("../modelo/usuario.php");
+	include_once("../modelo/empresa.php");
+	$objUsuario=new Usuario();
+	$objEmpresa=new Empresa();
 
-	class usuarioControlador{
-		private $objUsuario;
-		private $objConexion;
-
-
-		
+	function validarDatos($validacion){
+		if ($_POST['txtCedula']=="") {
+			return "La cedula es obligatoria";
+		}else{
+			if ($validacion) {
+				if (mysqli_num_rows($objUsuario->consultarCedula($_POST['txtCedula']))!=0) {
+					return "Ya exite un usuario registrado con la misma cedula";
+				}
+			}else{
+				if (mysqli_num_rows($objUsuario->consultarCedula($_POST['txtCedula']))==0) {
+					return "No exite un usuario registrado con la misma cedula";
+				}
+			}
+		}
+		if ($_POST['txtNombre']=="") {
+			return "El nombre es obligatorio";
+		}else if ($_POST['comboSexo']=="") {
+			return "El sexo es obligatorio";
+		}else if ($_POST['comboEstadoCivil']=="") {
+			return "El estado civil es obligatorio";
+		}else if ($_POST['txtFechaNacimiento']=="") {
+			return "La fecha de nacimiento es obligatorio"
+		}else if ($_POST['txtPersonasDepende']=="") {
+			return "La cantidad de personas que dependen es obligatoria";
+		}elseif ($_POST['comboNivelEstudio']=="") {
+			return "El nivel de estudio es obligatorio";
+		}elseif ($_POST['txtProfesion']=="") {
+			return "La profesion es obligatoria";
+		}else if ($_POST['comboDepartamentoResidencia']=="") {
+			return "El departamento de residencia es obligatorio";
+		}elseif ($_POST['comboCiudadResidencia']=="") {
+			return "La ciudad de residencia es obligatoria";
+		}elseif ($_POST['comboEstrato']=="") {
+			return "El estrato es obligatorio";
+		}elseif ($_POST['comboTipoVivienda']=="") {
+			return "El tipo de vivienda es obligatorio";
+		}elseif ($_POST['comboDepartamentoTrabajo']=="") {
+			return "El departamento de trabajo es obligatorio";
+		}elseif ($_POST['comboCiudadTrabajo']=="") {
+			return "La ciudad de trabajo es obligatoria";
+		}else if ($_POST['comboEmpresa']=="") {
+			return "La empresa es obligatoria";
+		}else if (mysqli_num_rows($objEmpresa->consultarNit($_POST['comboEmpresa']))==0) {
+			return "La empresa seleccionada no esta registrada";
+		}elseif ($_POST['txtYearsTrabajo']=="") {
+			return "La cantidad de años de trabajo es obligatoria";
+		}elseif ($_POST['txtDepartamentoLaboral']=="") {
+			return "El departamento laboral es obligatorio";
+		}elseif ($_POST['txtCargo']=="") {
+			return "El cargo es obligatorio";
+		}elseif ($_POST['comboTipoCargo']=="") {
+			return "El tipo de cargo es obligatorio";
+		}elseif ($_POST['txtYearsCargo']=="") {
+			return "La cantidad de años en el cargo es obligatoria";
+		}elseif ($_POST['txtHorasTrabajo']=="") {
+			return "La cantidad de horas de trabajo al dia es obligatoria";
+		}elseif ($_POST['comboTipoContrato']=="") {
+			return "El tipo de contrato es obligatorio";
+		}elseif ($_POST['comboTipoSalario']=="") {
+			return "El tipo de salario es obligatorio";
+		}else{
+			return true;
+		}
 	}
+
+
+	function registrar(){
+		$val=validarDatos(true);
+		if ($val) {
+			$objUsuario->registrar($_POST['txtCedula'],$_POST['comboEmpresa'],$_POST['txtNombre'], $_POST['comboSexo'],$_POST['comboEstadoCivil'],$_POST['txtFechaNacimiento'],$_POST['txtPersonasDepende'],$_POST['comboDepartamentoResidencia'],$_POST['comboCiudadResidencia'],$_POST['comboEstrato'], $_POST['comboTipoVivienda'],$_POST['comboNivelEstudio'],$_POST['txtProfesion'],$_POST['comboDepartamentoTrabajo'],$_POST['comboCiudadTrabajo'],$_POST['txtYearsTrabajo'],$_POST['txtCargo'],$_POST['comboTipoCargo'],$_POST['txtYearsCargo'],$_POST['txtDepartamentoLaboral'], $_POST['comboTipoContrato'],$_POST['txtHorasTrabajo'],$_POST['comboTipoSalario']);
+		}else{
+			echo $val;
+		}
+	}
+
+	function modificar(){
+		$val=validarDatos(false);
+		if ($val) {
+			$objUsuario->modificar($_POST['txtCedula'],$_POST['comboEmpresa'],$_POST['txtNombre'], $_POST['comboSexo'],$_POST['comboEstadoCivil'],$_POST['txtFechaNacimiento'],$_POST['txtPersonasDepende'],$_POST['comboDepartamentoResidencia'],$_POST['comboCiudadResidencia'],$_POST['comboEstrato'], $_POST['comboTipoVivienda'],$_POST['comboNivelEstudio'],$_POST['txtProfesion'],$_POST['comboDepartamentoTrabajo'],$_POST['comboCiudadTrabajo'],$_POST['txtYearsTrabajo'],$_POST['txtCargo'],$_POST['comboTipoCargo'],$_POST['txtYearsCargo'],$_POST['txtDepartamentoLaboral'], $_POST['comboTipoContrato'],$_POST['txtHorasTrabajo'],$_POST['comboTipoSalario'],$_POST['comboEstado']);
+		}else{
+			echo $val;
+		}
+	}
+
+	function listar(){
+		print_r($objUsuario->listar());
+	}
+
+	function consultarCedula(){
+		print_r($objUsuario->consultarCedula($_POST['txtCedula']));
+	}
+
+	function consultarNombre(){
+		print_r($objUsuario->consultarNombre($_POST['txtConsultaNombre']));
+	}
+
+	function consultarEmpresa(){
+		print_r($objUsuario->consultarEmpresa($_POST['comboEmpresa']));
+	}
+
 
 ?>
--- modelo/usuario.php
+++ modelo/usuario.php
@@ -1,37 +1,39 @@
 <?php
-	class Usuario{
-		private $cedula_usuario;
-		private $id_empresa_usuario;
-		private $nombre_usuario;
-		private $sexo_usuario;
-		private $estado_civil_usuario;
-		private $fecha_nacimiento_usuario;
-		private $personas_depende_usuario;
-		private $departamento_residencia_usuario;
-		private $ciudad_residencia_usuario;
-		private $estrato_usuario;
-		private $tipo_vivienda_usuario;
-		private $nivel_estudio_usuario;
-		private $profesion_usuario;
-		private $departamento_trabajo_usuario;
-		private $ciudad_trabajo_usuario;
-		private $years_trabajo_usuario;
-		private $cargo_usuario;
-		private $tipo_cargo_usuario;
-		private $years_cargo_usuario;
-		private $departamento_laboral_usuario;
-		private $tipo_contrato_usuario;
-		private $horas_dia_trabajo_usuario;
-		private $tipo_salario_usuario;
-		private $tipo_usuario;
-		private $estado_usuario;
+	include_once("conexion.php");
 
-		public function get($campo){
-			return $this->$campo;
+	class Usuario{
+		private $objConexion=new Conexion();
+
+		public function registrar($cedula, $idEmpresa, $nombre, $sexo, $estadoCivil, $fechaNacimiento, $personasDepende, $departamentoResidencia, $ciudadResidencia, $estrato, $tipoVivienda, $nivelEstudio, $profesion, $departamentoTrabajo, $ciudadTrabajo, $yearsTrabajo, $cargo, $tipoCargo, $yearsCargo, $departamentoLaboral, $tipoContrato, $horasTrabajo, $tipoSalario){
+
+			$consulta="insert into usuario values(".$cedula.",".$idEmpresa.",'".$nombre."','".$sexo."','".$estadoCivil."','".$fechaNacimiento."',".$personasDepende.",'".$departamentoResidencia."','".$ciudadResidencia."','".$estrato."','".$tipoVivienda."','".$nivelEstudio."','".$profesion."','".$departamentoTrabajo."','".$ciudadTrabajo."',".$yearsTrabajo.",'".$cargo."','".$tipoCargo."',".$yearsCargo.",'".$departamentoLaboral."','".$tipoContrato."',".$horasTrabajo.",'".$tipoSalario."','Usuario','activo')";
+			$this->objConexion->consultaSimple($consulta);
 		}
 
-		public function set($campo,$valor){
-			$this->$campo=$valor;
+		public function modificar($cedula, $idEmpresa, $nombre, $sexo, $estadoCivil, $fechaNacimiento, $personasDepende, $departamentoResidencia, $ciudadResidencia, $estrato, $tipoVivienda, $nivelEstudio, $profesion, $departamentoTrabajo, $ciudadTrabajo, $yearsTrabajo, $cargo, $tipoCargo, $yearsCargo, $departamentoLaboral, $tipoContrato, $horasTrabajo, $tipoSalario, $estado){
+
+			$consulta="update usuario set id_empresa_usuario = ".$idEmpresa.", nombre_usuario = '".$nombre."', sexo_usuario = '".$sexo."', estado_civil_usuario = '".$estadoCivil."', fecha_nacimiento_usuario = '".$fechaNacimiento."', personas_depende_usuario = ".$personasDepende.", $departamento_residencia_usuario = '".$departamentoResidencia."', ciudad_residencia_usuario = '".$ciudadResidencia."', estrato_usuario = '".$estrato."', tipo_vivienda_usuario = '".$tipoVivienda."', nivel_estudio_usuario = '".$nivelEstudio."', profesion_usuario = '".$profesion."', departamento_trabajo_usuario = '".$departamentoTrabajo."', ciudad_trabajo_usuario = '".$ciudadTrabajo."', years_trabajo_usuario = ".$yearsTrabajo.", cargo_usuario = '".$cargo."', tipo_cargo_usuario = '".$tipoCargo."', departamento_laboral_usuario = '".$departamentoLaboral."', tipo_contrato_usuario = '".$tipoContrato."', horas_dia_trabajo_usuario = ".$horasTrabajo.", tipo_salario_usuario = '".$tipoSalario."', estado_usuario = '".$estado."' where cedula_usuario = ".$cedula;
+			$this->objConexion->consultaSimple($consulta);		
+		}
+
+		public function listar(){
+			$consulta="select * from usuario";
+			$this->objConexion->consultaRetorno($consulta);
+		}
+
+		public function consultarCedula($cedula){
+			$consulta="select * from usuario where cedula_usuario = ".$cedula;
+			$this->objConexion->consultaRetorno($consulta);
+		}
+
+		public function consultarNombre($nombre){
+			$consulta="select * from usuario where nombre_usuario like '".$cedula."%'";
+			$this->objConexion->consultaRetorno($consulta);
+		}
+
+		public function consultaEmpresa($idEmpresa){
+			$consulta="select * from usuario where id_empresa_usuario = ".$idEmpresa;
+			$this->objConexion->consultaRetorno($consulta);
 		}
 
 	}
